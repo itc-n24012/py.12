600b4193b0e15e64e066e241fe0fc9a1633f1604
n24012 <n24012@std.it-college.ac.jp>
2024-07-24 06:03:48 +0000

    kadai

---
+++ 213-1.py
@@ -0,0 +1,10 @@
+quu = []
+quu.append(1)
+print('quu:', quu)
+quu.append(2)
+print('quu:', quu)
+print('pop from quu 1 value:', quu.pop(0))
+print('pop from quu 2 value:', quu.pop(0))
+print('quu:', quu)
+
+
