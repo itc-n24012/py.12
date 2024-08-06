4776e5462b6677100fa9c28df594bf3206f2faef
n24012 <n24012@std.it-college.ac.jp>
2024-08-06 06:15:15 +0000

    kadai

---
+++ 284.py
@@ -0,0 +1,19 @@
+class person:
+    def __init__(self,
+                 name = '',
+                 nationality = '',
+                 birth = '',
+                 address = ''):
+        self.name = name
+        self.nationality = nationality
+        self.birth = birth
+        self.address = address
+
+    def show_attributes(self):
+        print('名前:', self.name)
+        print('国籍:', self.nationality)
+        print('生まれた年:', self.birth)
+        print('住んでいる所:', self.address)
+
+selly = person('アン ジョンファン', '韓国', '1998', '韓国のソウル？')
+selly.show_attributes()
