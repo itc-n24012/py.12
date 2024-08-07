cb1f6e4e66b3f21d73133ef8477350a724857d7c
n24012 <n24012@std.it-college.ac.jp>
2024-08-07 06:33:27 +0000

    kadai

---
+++ 312.py
@@ -0,0 +1,13 @@
+import random
+random.seed(1)
+msgs = ['Hi', 'Hello', 'Good morning', 'Good night', 'See you later', 'How are you', 'Have a good day']
+with open('some.txt','w') as f:
+    for i in range(1000000):
+        f.write('{}, {}\n'.format(i, random.choice(msgs)))
+
+f = open('some.txt')
+lines = ''
+for i in range(3):
+    lines += f.readline()
+print(lines)
+f.close()
